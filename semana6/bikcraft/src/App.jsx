b477354e73df2477bd24d7f80026673cc5b10e01
pricbnll <pricbnll@gmail.com>
2024-07-09 15:49:51 -0300

    feat: [M2S06] Ex 4 - Construir um componente de footer para aplicação

--- semana6/bikcraft/src/App.jsx
+++ semana6/bikcraft/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Header></Header>
+      <Footer></Footer>
     </>
   )
 }
---
+++ semana6/bikcraft/src/components/Footer.jsx
@@ -0,0 +1,52 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <>
+        <footer  className='footer'>
+                     
+            <img src="https://www.origamid.com/projetos/bikcraft/img/bikcraft.svg" alt="Logo Bikcraft" />
+        
+            <div className='contact'>
+                <h3>CONTATO</h3>
+                <ul>
+                    <li>+55 21 9999-9999</li>
+                    <li>contato@bikcraft.com</li>
+                    {/* linha */}
+                    <hr class="separator"/>
+                    <li>Rua Ali Perto, 42 - Botafogo</li>
+                    <li>Rio de Janeiro - RJ</li>
+                    {/* linha */}
+                    <hr class="separator"/>
+                </ul>          
+                  
+                <div className="social-midia">
+                    <a href="./">
+                        <img src="https://www.origamid.com/projetos/bikcraft/img/redes/instagram.svg" alt="Logotipo do Instagram" />
+                    </a>
+                    <a href="./">
+                        <img src="https://www.origamid.com/projetos/bikcraft/img/redes/facebook.svg" alt="Logotipo do Facebook" />
+                    </a>
+                    <a href="./">
+                        <img src="https://www.origamid.com/projetos/bikcraft/img/redes/youtube.svg   " alt="Logotipo do Youtube" />
+                    </a>
+                   
+                </div>
+            </div>
+
+            <nav>
+                <h3>INFORMAÇÕES</h3>
+                <ul>
+                    <li>Bicicletas</li>
+                    <li>Seguros</li>
+                    <li>Contato</li>
+                    <li>Termos e Condições</li>
+                </ul>
+            </nav>
+            <p>Bikcraft © Alguns direitos reservados.</p>
+        </footer>
+        </>
+    )
+}
+
+export default Footer
